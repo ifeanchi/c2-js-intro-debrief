f350c8602142c3ece420ddf9a9c717a6328005a3
ifeanchi <stanley.enyinnaya21@gmail.com>
2021-04-26 10:55:24 -0500

    code for the last assignment done

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ else if (myNumber%3 === 0){ console.log('Gen');}
 else if (myNumber%5 === 0){ console.log('Buzz');}
 else {console.log(myNumber);}*/
 
-let storeItem = 'shoes';
+/*let storeItem = 'shoes';
 switch (storeItem){ case 'jeans': console.log('shoes are $50');
 break;
 case 'hat': console.log('hats are $12');
@@ -16,4 +16,10 @@ break;
 case 'socks': console.log('socks are $2');
 break;
 default: console.log('invalid item');
-break;}
+break;}*/
+
+
+let minNumber = 50;
+let maxNumber = 100;
+console.log(Math.floor(Math.random() * (minNumber - maxNumber) + minNumber));
+
