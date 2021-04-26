ca9f2c666843ba1395e52f46f914ff271b657bc0
ifeanchi <stanley.enyinnaya21@gmail.com>
2021-04-26 10:35:20 -0500

    Ecommerce code added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-let myNumber = 20;
+
+/*let myNumber = 20;
 if (typeof myNumber !== "number") {console.log('this is not a number');}
 else if (myNumber%15 === 0){ console.log('Genbuzz');}
 else if (myNumber%3 === 0){ console.log('Gen');}
 else if (myNumber%5 === 0){ console.log('Buzz');}
-else {console.log(myNumber);}
+else {console.log(myNumber);}*/
+
+let storeItem = 'shoes';
+switch (storeItem){ case 'jeans': console.log('shoes are $50');
+break;
+case 'hat': console.log('hats are $12');
+break;
+case 'shoes': console.log('shoes are $50');
+break;
+case 'socks': console.log('socks are $2');
+break;
+default: console.log('invalid item');
+break;}
